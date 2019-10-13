a0c5ea8b75139d526db8618cf1d59e584aa16d34
Karl Jerrenny Ojeda <karlojeda@yahoo.com>
2019-10-13 05:46:24 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -16,13 +16,13 @@ def hopper
         }
      }
 
-
+programmer_hash[:grace_hopper]
 end
 
 def alan_kay_is_known_for
 	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
 	
-	programmer_hash[:grace_hopper]
+	
 	
 	programmer_hash = 
  		{
